8ccfbefacf8ce9058ad82f403aeff2fdb8838708
TWLaoluo <66513074+TWLaoluo@users.noreply.github.com>
2020-06-21 16:22:13 +0800

    03

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-$('#banner').slick({
-    autoplay:true,
-});
+// 初始化
+$(document).ready(function(){
+    AOS.init();
 
-$('brand').slick();
+// ,結尾
+
+    $('#banner').slick({
+        autoplay:true,
+        autoplaySpeed: 1000,
+        arrows:false,
+    });
+
+    $('#brand').slick({
+        slidesToShow:5,
+        slidesToScroll:2,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots:true,
+        arrows:false,
+        variableWidth:true
+    });
+});
